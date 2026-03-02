8714aab2cbcbe29ba3edd377187144a15eb8d83a
28 <w2607674311@gmail.com>
2026-03-02 20:30:48 +0800

    整合Mybatis，完成数据库联调

--- xiaolvshu-auth/src/main/java/com/yating/xiaolvshu/auth/XiaolvshuAuthApplication.java
+++ xiaolvshu-auth/src/main/java/com/yating/xiaolvshu/auth/XiaolvshuAuthApplication.java
@@ -1,9 +1,12 @@
 package com.yating.xiaolvshu.auth;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yating.xiaolvshu.auth.domain.mapper")
 public class XiaolvshuAuthApplication {
 
     public static void main(String[] args) {
--- xiaolvshu-auth/src/test/java/com/yating/xiaolvshu/auth/XiaolvshuAuthApplicationTests.java
+++ xiaolvshu-auth/src/test/java/com/yating/xiaolvshu/auth/XiaolvshuAuthApplicationTests.java
@@ -1,13 +1,45 @@
 package com.yating.xiaolvshu.auth;
 
+import com.yating.framework.common.util.JsonUtils;
+import com.yating.xiaolvshu.auth.domain.dataobject.UserDO;
+import com.yating.xiaolvshu.auth.domain.mapper.UserDOMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
+@Slf4j
 class XiaolvshuAuthApplicationTests {
+
+    @Resource
+    private UserDOMapper userDOMapper;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testInsert(){
+        UserDO userDO = new UserDO();
+        userDO.setUsername("dsdf");
+        userDO.setCreateTime(LocalDateTime.now());
+        userDO.setUpdateTime(LocalDateTime.now());
+
+        userDOMapper.insert(userDO);
+    }
+
+
+    /**
+     * 查询数据
+     */
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 4 的记录
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
     }
 
 }
